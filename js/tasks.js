891b44d84f8bfd2bd2ac8e0baf14aae88257573c
Valdis <vladkoznecov@gmail.com>
2022-09-21 11:59:18 +0300

    new task

--- js/tasks.js
+++ js/tasks.js
@@ -226,11 +226,6 @@ console.log(reverse(someString)); */
 /* const baseCurrencies = ['USD', 'EUR'];
 const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 
-
-
-
-
-
 function availableCurr(arr, missingCurr) {
     if(arr.length == 0 ){
         return 'Нет доступных валют';
@@ -250,7 +245,66 @@ function availableCurr(arr, missingCurr) {
 console.log(availableCurr(['USD', 'EUR', 'UAH', 'RUB', 'CNY'], 'CNY'));
  */
 
+/* 
+У вас есть небольшой кусочек данных о торговом центре, которые записаны в объекте shoppingMallData.
+ Они содержат массив с данными о магазинах, где указана длина и ширина помещения; высоту помещения;
+ стоимость отопления за 1 кубический метр и бюджет на оплату отопления за месяц.
 
+Основная задача - это написать функцию isBudgetEnough, которая буде возвращать строку. 
+Если бюджета хватает для отопления всего объема торгового центра - выводится 'Бюджета достаточно', если нет - 'Бюджета недостаточно'. 
+И все 
+
+Но эта задача содержит несколько подзадач внутри:
+
+- вычисление общей площади всех магазинов, которая вычисляется как длина магазина, умноженная на его ширину;
+
+- вычисление общего объема торгового центра, так как цена отопления указана в кубических метрах;
+
+- определение того, хватает ли бюджета на оплату такого объема;
+
+- все числа идут без единиц измерения для упрощения, просто цифры и все;
+
+- функция должна продолжать работать, даже если изменяется количество магазинов, высота, бюджет или подставляется вообще другой объект */
+/* 
+let shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 51000
+};
+
+
+function isBudgetEnough(data){
+    const shopsS = [...data.shops];
+    let x = 0;
+    for(let i = 0; i<shopsS.length; i++){
+        x += shopsS[i].width * shopsS[i].length;
+    }
+    x = x * shoppingMallData.height * shoppingMallData.moneyPer1m3;
+    if(x<=shoppingMallData.budget){
+        return 'Бюджета достаточно';
+        } 
+        return 'Бюджета недостаточно'; 
+}
+
+console.log(isBudgetEnough(shoppingMallData)); */
 
 
 
